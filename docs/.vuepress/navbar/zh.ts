1620cfee95a00a2604b25fe291c58dd2efc7e952
Luo <943215317@qq.com>
2023-02-17 14:13:51 +0800

    更新部署

--- docs/.vuepress/navbar/zh.ts
+++ docs/.vuepress/navbar/zh.ts
@@ -1,32 +1,33 @@
-import { navbar } from "vuepress-theme-hope";
+import {navbar} from "vuepress-theme-hope";
 
 export const zhNavbar = navbar([
-  "/",
-  // { text: "案例", icon: "discover", link: "/demo/" },
-  // {
-  //   text: "指南",
-  //   icon: "creative",
-  //   prefix: "guide/",
-  //   children: [
-  //     {
-  //       text: "Bar",
-  //       icon: "creative",
-  //       prefix: "bar/",
-  //       children: ["baz", { text: "...", icon: "more", link: "" }],
-  //     },
-  //     {
-  //       text: "Foo",
-  //       icon: "config",
-  //       prefix: "foo/",
-  //       children: ["ray", { text: "...", icon: "more", link: "" }],
-  //     },
-  //   ],
-  // },
-  // {
-  //   text: "V2 文档",
-  //   icon: "note",
-  //   link: "https://theme-hope.vuejs.press/zh/",
-  // },
-  { text: "Python", icon: "python", link: "/python/" },
+    "/",
+    // { text: "案例", icon: "discover", link: "/demo/" },
+    // {
+    //   text: "指南",
+    //   icon: "creative",
+    //   prefix: "guide/",
+    //   children: [
+    //     {
+    //       text: "Bar",
+    //       icon: "creative",
+    //       prefix: "bar/",
+    //       children: ["baz", { text: "...", icon: "more", link: "" }],
+    //     },
+    //     {
+    //       text: "Foo",
+    //       icon: "config",
+    //       prefix: "foo/",
+    //       children: ["ray", { text: "...", icon: "more", link: "" }],
+    //     },
+    //   ],
+    // },
+    // {
+    //   text: "V2 文档",
+    //   icon: "note",
+    //   link: "https://theme-hope.vuejs.press/zh/",
+    // },
+    {text: "Python", icon: "python", link: "/python/"},
+    {text: "Markdown 神仙教程", icon: "markdown", link: "/markdown"},
 
 ]);
